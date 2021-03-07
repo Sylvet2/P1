9c15fc7f33c6f6b61586b23532508675469a05de
USUARIO <USUARIO@DESKTOP-FT3D548>
2021-03-06 22:31:00 -0500

    comrpobarfjfh

--- src/ProyectoV1/Usuarios.java
+++ src/ProyectoV1/Usuarios.java
@@ -104,7 +104,7 @@ public class Usuarios extends javax.swing.JPanel {
         DatosUsuario Regresar = new DatosUsuario();
         Regresar.setVisible(true);
         
-       //jdjfsfsfh
+       //jdjfsfsfhdsskg
         
         
     }//GEN-LAST:event_btnAtrasActionPerformed
