f35d6954a47af85634a5b9896e2dc766eba16068
Usuario <Usuario@DESKTOP-FNQGELG>
2021-03-11 20:55:39 -0500

    interfaz usuario hombre y usuario mujer

--- src/ProyectoV1/InterfazHombre.java
+++ src/ProyectoV1/InterfazHombre.java
@@ -27,17 +27,65 @@ public class InterfazHombre extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        btnPrendasBásicasHombre1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+        btnPrendasBásicasHombre = new javax.swing.JButton();
+        btnAtrasHombre = new javax.swing.JButton();
+        btnInviernoHombre1 = new javax.swing.JButton();
+        btnVeranoHombre1 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
+        btnPrendasBásicasHombre1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnPrendasBásicasHombre1.setText("PRENDAS BÁSICAS");
+        btnPrendasBásicasHombre1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnPrendasBásicasHombre.setFont(new java.awt.Font("Times New Roman", 0, 12)); // NOI18N
+        btnPrendasBásicasHombre.setForeground(new java.awt.Color(51, 51, 51));
+        btnPrendasBásicasHombre.setText("Atrás");
+        btnPrendasBásicasHombre.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnPrendasBásicasHombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, 80, 30));
+
+        btnAtrasHombre.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnAtrasHombre.setForeground(new java.awt.Color(51, 51, 51));
+        btnAtrasHombre.setText("PRENDAS BÁSICAS");
+        btnAtrasHombre.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnAtrasHombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 180, 140, 30));
+
+        btnInviernoHombre1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnInviernoHombre1.setForeground(new java.awt.Color(51, 51, 51));
+        btnInviernoHombre1.setText("INVIERNO");
+        btnInviernoHombre1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnInviernoHombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 80, 100, 30));
+
+        btnVeranoHombre1.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnVeranoHombre1.setForeground(new java.awt.Color(51, 51, 51));
+        btnVeranoHombre1.setText("VERANO");
+        btnVeranoHombre1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnVeranoHombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 130, 100, 30));
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("Selecciona una temporada..");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 180, 30));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/nubehombre.jpg"))); // NOI18N
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 11, 399, 278));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
@@ -82,5 +130,13 @@ public class InterfazHombre extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAtrasHombre;
+    private javax.swing.JButton btnInviernoHombre1;
+    private javax.swing.JButton btnPrendasBásicasHombre;
+    private javax.swing.JButton btnPrendasBásicasHombre1;
+    private javax.swing.JButton btnVeranoHombre1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
--- src/ProyectoV1/InterfazMujer.java
+++ src/ProyectoV1/InterfazMujer.java
@@ -27,21 +27,72 @@ public class InterfazMujer extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
+        btnPrendasBasicasMujer = new javax.swing.JButton();
+        btnVeranoMujer = new javax.swing.JButton();
+        btnInviernoMujer = new javax.swing.JButton();
+        btnAtrasMujer = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        btnPrendasBasicasMujer.setFont(new java.awt.Font("Times New Roman", 0, 12)); // NOI18N
+        btnPrendasBasicasMujer.setForeground(new java.awt.Color(51, 51, 51));
+        btnPrendasBasicasMujer.setText("Atrás ");
+        btnPrendasBasicasMujer.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnPrendasBasicasMujer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnPrendasBasicasMujerActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnPrendasBasicasMujer, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 240, 70, 30));
+
+        btnVeranoMujer.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnVeranoMujer.setForeground(new java.awt.Color(51, 51, 51));
+        btnVeranoMujer.setText("INVIERNO");
+        btnVeranoMujer.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnVeranoMujer, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 80, 80, 30));
+
+        btnInviernoMujer.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnInviernoMujer.setForeground(new java.awt.Color(51, 51, 51));
+        btnInviernoMujer.setText("VERANO");
+        btnInviernoMujer.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnInviernoMujer, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 130, 80, 30));
+
+        btnAtrasMujer.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        btnAtrasMujer.setForeground(new java.awt.Color(51, 51, 51));
+        btnAtrasMujer.setText("PRENDAS BÁSICAS");
+        btnAtrasMujer.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        jPanel1.add(btnAtrasMujer, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 180, 140, 30));
+
+        jLabel2.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 51, 51));
+        jLabel2.setText("Seleccione una temporada...");
+        jPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, 20));
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/nubemujer.jpg"))); // NOI18N
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 350, 290));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 352, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 294, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnPrendasBasicasMujerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnPrendasBasicasMujerActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnPrendasBasicasMujerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -80,5 +131,12 @@ public class InterfazMujer extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAtrasMujer;
+    private javax.swing.JButton btnInviernoMujer;
+    private javax.swing.JButton btnPrendasBasicasMujer;
+    private javax.swing.JButton btnVeranoMujer;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
